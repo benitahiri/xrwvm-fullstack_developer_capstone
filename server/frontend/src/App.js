c8f99e7140443114fba83b95c35c4f24fb32e7c1
Beni <benitahiri@hotmail.com>
2024-08-03 19:50:40 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,18 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './Register';
+import Home from './Home'; // Assuming you have a Home component
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
